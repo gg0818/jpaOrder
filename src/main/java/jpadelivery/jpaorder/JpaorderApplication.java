96eabbb62abd2236dc7af622fb9ed5efeac1c0ae
JiHyun Jang <gihyun0818@gmail.com>
2022-03-02 22:49:59 +0900

    프로젝트 생성

---
+++ src/main/java/jpadelivery/jpaorder/JpaorderApplication.java
@@ -0,0 +1,13 @@
+package jpadelivery.jpaorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpadelivery/jpaorder/JpaorderApplicationTests.java
@@ -0,0 +1,13 @@
+package jpadelivery.jpaorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
